96405f47931495ccf3fc7a9519fbd88acd4aeaf8
Lal <nbaanbaa@gmail.com>
2022-11-05 14:51:39 +0800

    123456

--- 123/src/gogo2/run.java
+++ 123/src/gogo2/run.java
@@ -3,7 +3,12 @@ package gogo2;
 public class run {
 public static void main(String[] args) {
 	System.out.println("number2");
-}{
-	
+	System.out.println("number2");
+	System.out.println("number2");
+	System.out.println("number2");
+	System.out.println("number2");
+	System.out.println("number2");
 }
+//¦Û¤v
+
 }
